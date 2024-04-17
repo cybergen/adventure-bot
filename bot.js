b13782e4644bc61d3d6e2108e8a342254b4e3d28
vimes <vimes.stx@gmail.com>
2024-04-17 13:08:07 -0700

    Factored states into some constants. Moved api key to env variable

--- bot.js
+++ bot.js
@@ -8,6 +8,13 @@ const openai = new OpenAI();
 const initialJoinWindow = 20000;
 const stageResponseWindow = 6000;
 
+//possible states
+const IDLE_STATE = "idle";
+const ACTIVE_STATE = "active";
+const POST_STAGE_STATE = "post-stage";
+const INPUT_STAGE_STATE = "input-stage";
+
+//overall state tracking
 let currentState = "idle";
 let courseChannel = null;
 let currentStage = 0;
@@ -19,10 +26,10 @@ let courseHistory = [];
 //History of what has befallen each player over time
 let playerHistory = [];
 //Chat-like full text of current stage for coherency (resets for each stage)
-let currentStageContext = "";
+let currentStageContext;
 
 function resetCourse() {
-    currentState = "idle";
+    currentState = IDLE_STATE;
     currentStage = 0;
     currentStageContext = "";
     courseDescription = {};
@@ -37,11 +44,11 @@ client.once('ready', () => {
 client.on('messageCreate', async message => {
     if (message.author.bot) return;
 
-    if (currentState === "idle" && message.content.startsWith('!adventure')) {
+    if (currentState === IDLE_STATE && message.content.startsWith('!adventure')) {
       handleNewAdventure(message);
-    } else if (currentState === "input-stage" && courseDescription?.players?.includes(message.author.username)) {
+    } else if (currentState === INPUT_STAGE_STATE && courseDescription?.players?.includes(message.author.username)) {
       handleAdventureProgress(message.author.username, message.content);
-    } else if (currentState !== "idle" && courseDescription?.players?.includes(message.author.username)) {
+    } else if (currentState !== IDLE_STATE && courseDescription?.players?.includes(message.author.username)) {
       courseChannel.send("You can only take part in the adventure during challenge stages");
     }
 });
@@ -61,7 +68,7 @@ async function handleNewAdventure(message) {
   collector.on('end', collected => {
       const players = collected.map(reaction => reaction.users.cache.filter(u => !u.bot).map(user => user.username)).flat();      
       console.log(`Collected ${collected.size} items with players ${players}`);   
-      currentState = "active";
+      currentState = ACTIVE_STATE;
       currentStage = 0;
       courseChannel = message.channel;
       startCourse(params, players);
@@ -74,7 +81,7 @@ async function startCourse(prompt, players) {
   courseChannel.send(`DEBUG: Full course info ${JSON.stringify(courseDescription)}`);
   while (currentStage < courseDescription.stages) {    
     await startStage();
-    currentState = "post-stage";
+    currentState = POST_STAGE_STATE;
     await endStage();
     currentStage++;
   }
@@ -84,7 +91,7 @@ async function startCourse(prompt, players) {
 async function startStage() {
   currentStageContext = await getLLMStageDescription(courseDescription, courseHistory, playerHistory);
   courseChannel.send(currentStageContext);
-  currentState = "input-stage";
+  currentState = INPUT_STAGE_STATE;
   await delay(stageResponseWindow);
 }
 
@@ -110,7 +117,7 @@ async function endAdventure(channel) {
 }
 
 // Login to Discord with your app's token
-client.login('MTIyNjcyNjYxNDg4NTMzNTE2Mg.GtEOHd.XRZgKJGceKjP-T_5zaF-gDI41q-2sgzTewxvQE');
+client.login(process.env.DISCORD_API_KEY);
 
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -124,7 +131,7 @@ LLM Functions
 
 // Simulated function to get LLM stage description
 async function getLLMCourseDescription(prompt, players) {
-  const fullPrompt = courseDescriptionPrompt + prompt + ". The players are " + players.join(', ');
+  const fullPrompt = courseDescriptionSystemPrompt + prompt + ". The players are " + players.join(', ');
   // Simulated delay
   return await fetchOpenAIResponse(fullPrompt, "gpt-3.5-turbo", 512);
 }
@@ -158,7 +165,7 @@ async function getAdventureResults(courseDescription, courseHistory, playerHisto
 async function fetchOpenAIResponse(prompt, model, tokens) {
   try {    
     const completion = await openai.chat.completions.create({
-      messages: [{"role": "system", "content": courseDescriptionPrompt},
+      messages: [{"role": "system", "content": courseDescriptionSystemPrompt},
         {"role": "user", "content": prompt}],
       model: model,
       max_tokens: tokens
@@ -179,8 +186,7 @@ Prompt strings
 
 */
 
-const courseDescriptionPrompt = `
-System Prompt:
+const courseDescriptionSystemPrompt = `
 You are an AI that produces a data structure describing a text-based obstacle course, challenge gauntlet, or other fun experience consisting of multiple stages, for a set of players. Your input will be a list of player id's, a duration, and a theme prompt. Your outputted data structure should look like so:
 
 {
@@ -198,4 +204,45 @@ You are an AI that produces a data structure describing a text-based obstacle co
 }
 
 Assume that each stage takes around 1 minute or less, and set a stage count based on that. Be sure to come up with a witty name!
+`;
+
+const stageSystemPrompt = `
+You are a fun and sarcastic conversational bot that invents and presents a text-based challenge to a set of players (represented by user id's) and determines an outcome based on how they respond. The challenge is just one of many stages in a larger course. You take as input an overarching theme as well as a set of context info for the current state of the overall course and the players. For instance, some of the players may have already died on a previous stage, handle that (and any attempts to further interact) according to whatever is most entertaining/thematically consistent.
+
+Your initial input will look like so:
+
+Plan:
+{
+  name: 'Under the Mountains of Madness',
+  theme: 'Grim fantasy dungeon delve to save a sick elf',
+  stages: 10,
+  players: ['vimes', 'ghost_tree']
+}
+
+Course History:
+[
+  'Dungeon entrance encountered in a forest glade',
+  'Attacked by a series of spiders',
+  'Swinging blade trap across a narrow walkway',
+  'Skeletons and a molerat necromancer attack',
+  'Soul-eating machine that can dispense a crystal',
+]
+
+Player History: 
+
+{
+  player_id: 'vimes',
+  history: ['Tried and failed to lift a tree on stage 1', 'Executed a perfect backflip to save a friend on stage 3', 'Got a sword and a molerat corpse in stage 4', 'Broke sword in stage 6']
+}
+
+{
+  player_id: 'ghost_tree',
+  history: ['Ran all out in stage 2, becoming exhausted', 'Died to a naked molerat on stage 4']
+}
+
+First, you will describe the current challenge stage. Then, the players will respond with their actions, which you can treat as final or prompt for follow-up. When you receive a system message to describe outcome, you will post a text description of what happens to the players. When you receive a subsequent system call to update history, you will update the Course History and Player History objects to include the new results. Please output those history objects (and ONLY those history objects) in the same schema as above.
+`;
+
+const resultSummarizerSystemPrompt = `
+You are a fun and sarcastic conversational bot that declares the results of an overall challenge course undertaken by a set of players based on a set of input data about the theme, the stages of the course, and some player event/action logs. Depending on the challenge, there may be winners and losers, or they may only involve survival/failure. If appropriate to the theme, it may also make sense to call out consolation prizes or other distinctions among the players.
 `;
